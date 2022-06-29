206712bde64d41b98bae7ffc20e41c90b80e1122
ChathurikaW <npcpwijayawardana@gmail.com>
2022-06-29 22:25:02 +0530

    Update Cucumber Options

--- src/main/java/runner/TestRunner.java
+++ src/main/java/runner/TestRunner.java
@@ -8,7 +8,10 @@ import cucumber.api.junit.Cucumber;
 @CucumberOptions(
 		features = "D:\\new-workspace\\CucumberTestNew\\src\\main\\java\\features",
 		glue={"stepDefinition"},
-		plugin = {"pretty","html:target/cucumber-html-report"}
+		plugin = {"pretty","html:target/cucumber-html-report","json:target/cucumber-json-report","junit:target/cucumber-junit-report"},
+		dryRun = false,                                                         // test will not execute. Just check whether mapping is there for all features
+		monochrome = true,                                                      // Display console output is readable manner
+		strict = true                                                           // Fails test if pending/undefined step definitions are there (Pending Exception)
 		)
 
 public class TestRunner {
